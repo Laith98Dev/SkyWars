c1eeb84cab235d14de88f55f8409e7f513c8f22f
Laith Youtuber <49840784+laith98@users.noreply.github.com>
2019-08-05 11:22:10 -0700

    Update Main.php

--- src/LaithYT/Main.php
+++ src/LaithYT/Main.php
@@ -1,1 +1,34 @@
+<?php
 
+
+declare(strict_types=1);
+
+namespace LaithYT\SkyWars;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\tile\Sign;
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
+
+class Main extends PluginBase implements Listener {
+	
+	 public function onEnable(){
+	 	 $this->getServer()->getLogger()->info(" SkyWars Enabled ");
+      $this->getServer()->getLogger()->info(" Distribution is prohibited without permission of the owner ");
+     
+   }
+  
+  
